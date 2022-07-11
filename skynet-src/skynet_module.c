a54cd3bcf37fc23b795d5cb59e711df8f82bd8de
zhiskey <1274803758@qq.com>
2022-07-11 20:35:21 +0800

    modules模块

--- skynet-src/skynet_module.c
+++ skynet-src/skynet_module.c
@@ -21,6 +21,7 @@ struct modules {
 
 static struct modules * M = NULL;
 
+// 加载c服务的so文件
 static void *
 _try_open(struct modules *m, const char * name) {
 	const char *l;
@@ -28,33 +29,48 @@ _try_open(struct modules *m, const char * name) {
 	size_t path_size = strlen(path);
 	size_t name_size = strlen(name);
 
+    // 因为要用name替换问号，所以这里长度不需要加1（必定至少会删除一个问号）
 	int sz = path_size + name_size;
 	//search path
 	void * dl = NULL;
+    // 存放模块地址
 	char tmp[sz];
 	do
 	{
 		memset(tmp,0,sz);
+        // 去除path前的：;
 		while (*path == ';') path++;
+        // 如果path为空字符串，跳出循环
 		if (*path == '\0') break;
+        // 寻找第一个;的位置（多个path用;分隔）
 		l = strchr(path, ';');
+        // 如果没有分号就定位到字符串后一字节的地址
 		if (l == NULL) l = path + strlen(path);
+        // 计算第一个path长度
 		int len = l - path;
 		int i;
+        // 对问号前的内容进行拷贝
 		for (i=0;path[i]!='?' && i < len ;i++) {
 			tmp[i] = path[i];
 		}
+        // 用name替换问号，如果没有问号，就是将name拼在path最后（实际没用，因为path没有问号是错误的）
 		memcpy(tmp+i,name,name_size);
 		if (path[i] == '?') {
+            // path有问号，把问号后面的内容拼再name后面
 			strncpy(tmp+i+name_size,path+i+1,len - i - 1);
 		} else {
+            // path没问号，报错
 			fprintf(stderr,"Invalid C service path\n");
 			exit(1);
 		}
+        // 加载动态库，可能会失败
+        // flag见：https://blog.csdn.net/nicholas_dlut/article/details/101758101
 		dl = dlopen(tmp, RTLD_NOW | RTLD_GLOBAL);
+        // path指向字符串中下一个path（;后）的地址，循环直到成功加载动态库
 		path = l;
 	}while(dl == NULL);
 
+    // 加载失败，报错
 	if (dl == NULL) {
 		fprintf(stderr, "try open %s failed : %s\n",name,dlerror());
 	}
@@ -62,7 +78,7 @@ _try_open(struct modules *m, const char * name) {
 	return dl;
 }
 
-// 从modules中找到
+// 从modules中返回对应服务的skynet_module
 static struct skynet_module * 
 _query(const char * name) {
 	int i;
@@ -106,8 +122,12 @@ skynet_module_query(const char * name) {
 	if (result)
 		return result;
 
+    // 没找到对应服务
+
+    // 加自旋锁
 	SPIN_LOCK(M)
 
+    // 双重校验锁思想，防止多线程环境下重复加载skynet_module
 	result = _query(name); // double check
 
 	if (result == NULL && M->count < MAX_MODULE_TYPE) {
@@ -125,12 +145,13 @@ skynet_module_query(const char * name) {
 		}
 	}
 
+    // 解锁
 	SPIN_UNLOCK(M)
 
 	return result;
 }
 
-// 先判断没有create，没有的话就是一个全1的地址
+// 先判断没有create，没有的话就是一个二进制全1的地址
 void * 
 skynet_module_instance_create(struct skynet_module *m) {
 	if (m->create) {
